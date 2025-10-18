eb954838bd68c98a2b44a45a54e7b328996fde44
tayo <tiyo124@yahoo.com>
2025-10-18 01:38:20 -0400

    IAC ACTIONS

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,22 +1,22 @@
-#terraform {
- # required_version = "~> 1.0"
-  #backend "s3" {
-    #bucket = "infrabucket-iacgitops-eu-west-2"
-    #key    = "gitops-actions/state.tfstate"
-    #region = "eu-west-2"
-  #}
-  #required_providers {
-   # aws = {
-      #source  = "hashicorp/aws"
-      #version = "~> 5.7"
-    #}
-    #kubernetes = {
-    #  source  = "hashicorp/kubernetes"
-    #  version = "~> 2.3"
-    #}
-    #helm = {
-      #source  = "hashicorp/helm"
-      #version = "~> 2.16"
-    #}
-  #}
-#}
+terraform {
+ required_version = "~> 1.0"
+  backend "s3" {
+    bucket = "tee120485-s3-demo"
+    key    = "gitops-actions/state.tfstate"
+    region = "eu-west-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.16"
+    }
+  }
+}
